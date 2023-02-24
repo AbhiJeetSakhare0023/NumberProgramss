fc0748ba414d85396fb348ac04102f6acbe0482a
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-02-25 00:14:20 +0530

    Program which finds factors of given number.

---
+++ src/FactorsOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int num=sc.nextInt();
+        System.out.println("Factors are below :");
+        for(int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
