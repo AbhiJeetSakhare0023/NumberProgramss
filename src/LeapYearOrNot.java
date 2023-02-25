80744abf8ca52560b6fa132513c0ea38936be138
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-02-25 23:18:04 +0530

    Program which checks given year is leap or not

---
+++ src/LeapYearOrNot.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Year:");
+        int year=sc.nextInt();
+        if(year%400==0)
+        {
+        System.out.println(year+" is Leap Year.");
+        }
+        else if(year%4==0 && year%100!=0)
+        {
+
+            {
+                System.out.println(year+" is Leap Year.");
+            }
+        }
+        else
+        {
+            System.out.println(year+" is not Leap Year.");
+        }
+
+    }
+}
