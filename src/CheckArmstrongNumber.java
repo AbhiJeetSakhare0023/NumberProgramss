bd95e0c2629990a848b6dfb88412c67ee5b48811
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-02-26 23:42:52 +0530

    Program which checks given number is Armstrong or not for example  371 = 3*3*3+7*7*7+1*1*1= 371

---
+++ src/CheckArmstrongNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int num=sc.nextInt(); // 371
+        int temp=num;
+        int rem;
+        int sum=0;
+        while (num!=0)//true ,true ,true,false
+        {
+            rem=num%10;//1 ,7 ,3
+            sum=sum+(rem*rem*rem); // 1 ,1+7*7*7 ,(1+7*7*7)+3*3*3 = 1+343+27=371
+            num=num/10; // 37,3,0
+        }
+        if(temp==sum)
+        {
+            System.out.println(temp+" is Armstrong Number");
+        }
+        else
+        {
+            System.out.println(temp+" is not Armstrong Number");
+        }
+
+    }
+}
